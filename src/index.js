ff5adadf712f9847adc8e367847b4a58ce5a8552
Daniel Abdel <danielmalek101@gmail.com>
2022-11-14 19:11:42 -0300

    tt

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import joi from 'joi';
 
-
 const app = express();
+app.use(express(JSON))
+const db = mongoClient.db('batepapouol')
 
- 
+
 app.listen(5000)
